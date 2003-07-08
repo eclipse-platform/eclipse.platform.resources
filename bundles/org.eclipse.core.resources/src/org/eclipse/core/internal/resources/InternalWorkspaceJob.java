d1b39e55fb145a6c83ebf2ad63e97cd906a5ef5d
John Arthorne <johna>
2003-07-08 15:41:41 +0000

    Added workspace job and resource scheduling rule

---
+++ bundles/org.eclipse.core.resources/src/org/eclipse/core/internal/resources/InternalWorkspaceJob.java
@@ -0,0 +1,53 @@
+/**********************************************************************
+ * Copyright (c) 2003 IBM Corporation and others. All rights reserved.   This
+ * program and the accompanying materials are made available under the terms of
+ * the Common Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors: 
+ * IBM - Initial API and implementation
+ **********************************************************************/
+package org.eclipse.core.internal.resources;
+
+import org.eclipse.core.internal.utils.Policy;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.*;
+import org.eclipse.core.runtime.jobs.ISchedulingRule;
+import org.eclipse.core.runtime.jobs.Job;
+
+/**
+ * Batches the activity of a job as a single operation, without obtaining the workspace
+ * lock.
+ */
+public abstract class InternalWorkspaceJob extends Job {
+	public static ISchedulingRule newSchedulingRule(IResource resource) {
+		return new ResourceSchedulingRule(resource);
+	}
+	public InternalWorkspaceJob(String name) {
+		super(name);
+	}
+	public IStatus run(IProgressMonitor monitor) {
+		monitor = Policy.monitorFor(monitor);
+		Workspace workspace = (Workspace)ResourcesPlugin.getWorkspace();
+		try {
+			monitor.beginTask(null, Policy.totalWork);
+			try {
+				workspace.prepareOperation();
+				workspace.beginOperation(true);
+				runInWorkspace(Policy.subMonitorFor(monitor, Policy.opWork, SubProgressMonitor.PREPEND_MAIN_LABEL_TO_SUBTASK));
+				return Status.OK_STATUS;
+			} catch (OperationCanceledException e) {
+				workspace.getWorkManager().operationCanceled();
+				return Status.CANCEL_STATUS;
+			} finally {
+				workspace.endOperation(false, Policy.subMonitorFor(monitor, Policy.buildWork));
+			}
+		} catch (CoreException e)  {
+			return e.getStatus();
+		} finally {
+			monitor.done();
+		}
+	}
+	protected abstract IStatus runInWorkspace(IProgressMonitor monitor) throws CoreException;
+}
