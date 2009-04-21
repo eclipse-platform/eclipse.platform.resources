8f2b0bd23474eede87d701f49cbd09470137c119
John Arthorne <johna>
2009-04-21 16:15:10 +0000

    Bug 261225 Build before launch takes 10s for a runtime workbench

--- tests/org.eclipse.core.tests.resources/src/org/eclipse/core/tests/resources/perf/AllTests.java
+++ tests/org.eclipse.core.tests.resources/src/org/eclipse/core/tests/resources/perf/AllTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2006 IBM Corporation and others.
+ * Copyright (c) 2004, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -22,6 +22,7 @@ public class AllTests extends TestSuite {
 		suite.addTest(BenchFileStore.suite());
 		suite.addTest(BenchWorkspace.suite());
 		suite.addTest(BenchMiscWorkspace.suite());
+		suite.addTest(BuilderPerformanceTest.suite());
 		suite.addTest(MarkerPerformanceTest.suite());
 		suite.addTest(LocalHistoryPerformanceTest.suite());
 		suite.addTest(WorkspacePerformanceTest.suite());
